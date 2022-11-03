e70b0ddfaf9afde36e6674ee3287ba2e8d98728b
Atulananda Panigrahy <atula.panigrahy@gmail.com>
2022-11-03 19:36:28 +0530

    Basics of class and object

---
+++ object.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+class Person{
+  // by default memebers are private
+  char name[20];
+  int id;
+
+  public:
+    void getDetails(){
+      // function body
+    }
+};
+
+int main()
+{
+  Person p1;
+  return 0;
+}
