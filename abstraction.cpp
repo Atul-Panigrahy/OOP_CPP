5c70de759a370441dc9a3bbf8e1561618848afe7
Atulananda Panigrahy <atula.panigrahy@gmail.com>
2022-11-04 14:35:49 +0530

    Basic Abstraction in CPP

---
+++ abstraction.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/*
+    DATA ABSTRACTION:
+      1. this refers to providing only essential information about the data to the outside world,
+          hiding the background details or implementaions.
+      2. Variations: 
+         1. Abstraction using class
+         2. Abstraction using header files 
+            e.g: pow(x,n) function to calculate the power of x
+        3. Abstraction using access modifiers.
+           e.g . keeping the data members private
+       
+*/
+int main()
+{
+
+  return  0;
+}
