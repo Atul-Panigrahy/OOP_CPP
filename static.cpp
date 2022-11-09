459c71d5818aaa6f88a3d4307eaf73d63e923208
Atulananda Panigrahy <atula.panigrahy@gmail.com>
2022-11-09 20:32:43 +0530

    Test private static members

--- static.cpp
+++ static.cpp
@@ -2,11 +2,11 @@
 
 /*
   STATIC DATA MEMBER:
-     1. Declaring a data meber in a class as static gives it scope. i.e. the variable no longer remains 
-       specific and bound to one particular object instance. Thereafter, chaning the value from one instance 
+     1. Declaring a data meber in a class as static gives it scope. i.e. the variable no longer remains
+       specific and bound to one particular object instance. Thereafter, chaning the value from one instance
        reflects over all instances.
-    2. They are created for one class and for every object shares them. 
-  
+    2. They are created for one class and for every object shares them.
+
   STATIC MEMBER FUNCTIONS:
     1.  similar to static data members. They are allowed to access only other static fields (data and members)
     2. can't access non-static members
@@ -14,39 +14,53 @@
     4. no concept of static constructor
 */
 
+// // STATIC DATA MEMBER 
 class Test
 {
-  public:
-    static int x;
-    
+private:
+  static int privateStaticVariable;
+
+public:
+  static int publicStaticVariable;
+
+  Test()
+  {
+    privateStaticVariable = 0;
+    publicStaticVariable = 0;
+  }
 };
 
-int Test::x = 1;
+int Test::publicStaticVariable = 1;
+int Test::privateStaticVariable = 3; // gives no error 
 
 int main()
 {
-  Test t1,t2;
+  Test t1, t2;
+  std::cout << "PUBLIC:"
+            << "\n";
   std::cout << "Before Update: \n";
-  std::cout << "Access from instance " << t1.x << "\n";
-  std::cout << "Access from class directly " << Test::x << "\n";
-  Test::x = 5;
-
+  std::cout << "Access from instance " << t1.publicStaticVariable << "\n";
+  std::cout << "Access from class directly " << Test::publicStaticVariable << "\n";
+  Test::publicStaticVariable = 5;
 
   std::cout << "After Update: \n";
-  std::cout << "Access from instance " << t1.x << "\n";
-  std::cout << "Access from class directly " << Test::x << "\n";
+  std::cout << "Access from instance " << t1.publicStaticVariable << "\n";
+  std::cout << "Access from class directly " << Test::publicStaticVariable << "\n";
 
-  return 0; 
+  // // INACCESSIBLE member
+  std::cout << "PRIVATE:"
+            << "\n";
+  // std::cout << Test::privateStaticVariable << "\n";
+  return 0;
 }
-
 
 /*
 OUTPUT:
 
 Before Update:
-Access from instance 1      
+Access from instance 1
 Access from class directly 1
 After Update:
-Access from instance 5      
+Access from instance 5
 Access from class directly 5
 */
